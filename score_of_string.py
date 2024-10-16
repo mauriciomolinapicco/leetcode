05203dab365d5c6ef52cb51928c02be9f73b6626
mauriciomolinapicco <mauriscode@gmail.com>
2024-10-16 13:37:52 -0300

    score of a string

---
+++ score_of_string.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        sum = 0
+        for i in range(len(s) - 1):
+            sum += abs(int(ord(s[i]) - ord(s[i+1])))
+        return sum
